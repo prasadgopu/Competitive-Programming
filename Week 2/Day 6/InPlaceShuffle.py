487a79bb5bd0bebc8c2a6bc104c5a98c2f5a7799
Lakshmi Prasad Gopu <lakshmiprasadgopu@LakshmiPrasads-MacBook-Pro.local>
2018-07-09 14:29:36 +0530

    Week 2

---
+++ Week 2/Day 6/InPlaceShuffle.py
@@ -0,0 +1,27 @@
+import random
+
+
+def get_random(floor, ceiling):
+    return random.randrange(floor, ceiling + 1)
+
+
+def shuffle(the_list):
+    if len(the_list) <= 1:
+        return the_list
+
+    l = len(the_list) - 1
+
+    for x in range(0, l):
+        rand = get_random(x, l)
+        if rand != x:
+            the_list[x], the_list[rand] = the_list[rand], the_list[x]
+    # Shuffle the input in place
+
+
+sample_list = [1, 2, 3, 4, 5]
+print('Sample list:', sample_list)
+
+print(
+'Shuffling sample list...',
+shuffle(sample_list))
+print(sample_list)
